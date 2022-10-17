ea47db07ac037de5c8720307b56c564bc626ec4f
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-10-18 01:29:39 +0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,12 +12,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.saws0i6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: "Access denied" })
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Acess forbiddenn" })
+        }
+        req.decoded = decoded;
+        next();
+    }
+    );
+}
+
 async function run() {
     try {
         await client.connect();
         const servicesCollection = client.db("cleanium").collection("services");
         const appointmentCollection = client.db("cleanium").collection("appointment");
         const estimationCollection = client.db("cleanium").collection("estimation");
+        const usersCollection = client.db("cleanium").collection("users");
         app.get('/services', async (req, res) => {
             // const result = await servicesCollection.find({}).toArray();
             const query = {};
@@ -43,6 +61,8 @@ async function run() {
         });
 
         app.get('/my-appointment', async (req, res) => {
+
+
             const query = req.query.email;
             console.log(query);
             const filter = { email: query };
@@ -60,6 +80,56 @@ async function run() {
 
         app.get('/estimation', async (req, res) => {
             const result = await estimationCollection.find().toArray();
+            res.send(result)
+        });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = await usersCollection.findOne({ email: email });
+            const isAdmin = query.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
+
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const data = req.body;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: data
+            };
+            const options = { upsert: true };
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send({ result, token })
+        });
+
+        app.get('/users', verifyJWT, async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.put('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        });
+
+        app.post('/add-service', async (req, res) => {
+            const data = req.body;
+            const result = await servicesCollection.insertOne(data);
+            res.send(result)
+        });
+
+        app.delete('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email }
+            const result = await usersCollection.deleteOne(filter);
             res.send(result)
         })
     }
